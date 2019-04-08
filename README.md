1c7cfd4b0f2d93d4250ae32d6a9e1bb03fc5dd96
David Yim <yimd85@gmail.com>
2019-04-08 18:57:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Wyng :: Front-End Developer Take Home
+
+An age gate landing page plugin thatmeets the following criteria:
+Month / Year Birthday Selector
+• Success / FailMessage
+• If user is 21 or older, show “welcome” success message.
+• If user is under the age of 21, show an error message.
+• A “remember me” check box that allows a successful user to return to the page and bypass the age gate.
+Bonus:
+• Upgrade the selector to include Monday /Day / Year.
+• Build this plugin to allow the code to be adjusted to easily account for any age requirement.
+<br>
+Created by [David Yim](https://github.com/yimd85).
+
+I am a react front end engineer however, I only made use of those libraries from the job description. 
+I am looking forward to hearing from you.
+
+## Local Installation
+
+```
+$ git clone 
+$ cd MFO
+double the html file
+```
+Try disabling popups
+Cookies are able to be save from local files. I made use of local storage as an alternative. Please be sure to clear them afterwards
+
+## Project Structure
+
+```
+.
+├── html.html     
+├── styles.css     
+└── index.js       
+
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+var min_age = 21;
+
+function underAge() {
+    localStorage.setItem("off_age", 'false'); 
+    $('#checkbox-value').text('Sorry you are underage!');
+    window.open("https://www.gotmilk.com/");
+}
+
+function ofAge() {
+    localStorage.setItem("off_age", 'true'); 
+    $('#checkbox-value').text('Welcome, you are of age!');
+    window.open("https://www.aarp.org/membership/");
+}
+
+$(document).ready(function(){
+    var today = new Date();
+    var todayMonth = today.getUTCMonth() +1;
+    var todayDate = today.getUTCDate();
+    var todayYear = today.getUTCFullYear();
+
+    if(localStorage.getItem('off_age') === 'true' && localStorage.getItem('remember_me') === 'true') {
+        ofAge();
+    }
+
+    $("#datepicker").datepicker({ 
+        autoclose: true, 
+        todayHighlight: true
+    });
+
+    $("#checkbox").on('change', function() {
+      if ($(this).is(':checked')) {
+        $(this).attr('value', 'true');
+      } else {
+        $(this).attr('value', 'false');
+      }
+    });
+
+    $("form").submit(function(event ){
+        var bday = $('#date').val().toString();
+        var yearThen = parseInt(bday.substring(6,10));
+        var monthThen = parseInt(bday.substring(0,2));
+        var dayThen = parseInt(bday.substring(3,5));
+        event.preventDefault();
+        if($('#date').val() === ''){ return $('#checkbox-value').text('Please enter a date')};
+        if( todayYear - yearThen < 21 ){
+            underAge();
+        } else if (todayYear - yearThen === 21 &&  todayMonth <  monthThen) {
+            underAge();
+        } else if (todayYear - yearThen === 21 &&  todayMonth <  monthThen ){
+            underAge();
+        } else if (todayYear - yearThen === 21 &&  todayMonth === monthThen  && todayDate <  dayThen) {
+            underAge();
+        } else {
+            ofAge();
+        };
+        localStorage.setItem("remember_me", $('#checkbox').val());
+    });
+});
+
+
+  
+
+
